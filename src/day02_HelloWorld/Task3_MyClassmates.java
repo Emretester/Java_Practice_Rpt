649e11126de72b023933c90f6fd739d829cc9964
Emretester <emreyeni2019@gmail.com>
2023-09-30 15:13:18 +0300

    Java Practice project created day02 finished

---
+++ src/day02_HelloWorld/Task3_MyClassmates.java
@@ -0,0 +1,15 @@
+package day02_HelloWorld;
+
+public class Task3_MyClassmates {
+
+    public static void main(String[] args) {
+
+        System.out.println("Ali");
+        System.out.println("Veli");
+        System.out.println("Ahmet");
+        System.out.println("Mehmet");
+        System.out.println("Ayse");
+
+
+    }
+}
